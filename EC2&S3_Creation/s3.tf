2087c7163f8f7bc31a7f10b1e36822d676be6554
alabens <ala.bensalah@ensi-uma.tn>
2020-04-12 20:46:29 +0100

    final version

--- EC2&S3_Creation/s3.tf
+++ EC2&S3_Creation/s3.tf
@@ -1,7 +1,25 @@
 resource "aws_s3_bucket" "mybucket-c29df123" {
   bucket = "mybucket-c29df123"
-  acl    = "public"
+  acl    = "public-read"
   force_destroy  = "true"
+  policy = "${file("policy.json")}"
+
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
 
   tags = {
     Name = "MyBucket"
